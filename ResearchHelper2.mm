3cff822efc80d1c02b7187ee4081a3d4ad983554
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-02 13:14:38 +0900

    master
    v.1
    d.2
    
    d---------------------------
    1. Migrated => Article
    
    n---------------------------

---
+++ ResearchHelper2.mm
@@ -0,0 +1,24 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1341201131859" ID="ID_711108113" MODIFIED="1341202237031" TEXT="ResearchHelper2">
+<node CREATED="1341201185296" ID="ID_571476456" MODIFIED="1341201187125" POSITION="right" TEXT="SETUP">
+<node CREATED="1341201187796" ID="ID_1615505197" MODIFIED="1341202256765" TEXT="rails new">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341202241953" ID="ID_1502225904" MODIFIED="1341202248187" TEXT="Change root path">
+<node CREATED="1341202294312" ID="ID_1496598417" MODIFIED="1341202315968" TEXT="routes.rb">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341202296078" ID="ID_832153778" MODIFIED="1341202315968" TEXT="index.html">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341202321140" ID="ID_415229528" MODIFIED="1341202325875" TEXT="Access page">
+<node CREATED="1341202336500" ID="ID_902129707" MODIFIED="1341202345500" TEXT="Local">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1341202339453" ID="ID_1362050781" MODIFIED="1341202341296" TEXT="Remote"/>
+</node>
+</node>
+</node>
+</node>
+</map>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ResearchHelper2::Application.routes.draw do
+  resources :articles
+
+  #
+  root :to => "articles#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
