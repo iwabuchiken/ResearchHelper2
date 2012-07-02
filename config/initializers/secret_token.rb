b93b1d6e0ae88e19ad374967a25fbe4be07a6a3b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-02 13:00:10 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResearchHelper2::Application.config.secret_token = 'a0bc21f899feefd667764d5d9649af3d96f63e9021f3c9499aa52bd6f9b067fe74ca98fcf4be5162222d76d17a7f960bde10f6363a5dffbf95515ea89f9b455a'
